6fe83ef01752734afc28660ff46ca4e033ad9917
Ben Dan <ben07servo@gmail.com>
2021-08-07 11:29:04 +0530

    First Python Project

---
+++ FirstPythonProject/main.py
@@ -0,0 +1,45 @@
+# # The below is an example of using an end variable which is an optional parameter in print function
+# print ("India is my", end= " ")
+# print ("Country", end=" Vah !!!")
+#
+# # variable reuse with multiple datatype works in python
+# a = 7
+# print (a)
+# a = "Benson"
+# print(a)
+
+#Lets see if contatenation of string an numbers work
+# print ("Benson" + 7)
+# Does not work
+
+# # Let see the example of global and local variables
+# f = 101
+# print (f)
+#
+# def someFunction():
+#     f = "Learning python for the first time"
+#     print (f)
+#
+# someFunction()
+# print (f)
+
+# # Below is an example of accessing global variable inside a function
+# f = 101
+# print (f)
+#
+# def someFunction():
+#     global f
+#     f = 102
+#     print (f)
+#
+# someFunction()
+# print(f)
+
+# # Variables could also be deleted Amazing
+# f = 103
+# print(f)
+#
+# del f
+# print(f)
+# # This gives an error saying variable is not defined
+
